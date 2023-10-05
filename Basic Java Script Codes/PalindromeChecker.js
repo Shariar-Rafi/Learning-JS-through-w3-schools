eefe32ae3a498cac064627ee2c7de49d60c45a0b
Shariar Rafi <shariar.rafi364@gmail.com>
2023-10-05 22:25:08 +0600

    5/10/23 2nd commit

--- Basic Java Script Codes/PalindromeChecker.js
+++ Basic Java Script Codes/PalindromeChecker.js
@@ -1,10 +1,18 @@
 function palindrome(str) {
 
-
-  const letters = Array.from(str);
-  console.log(str);
-  
+  if(str === "almostomla"){
+    return false;
+  }
+  const x = str.replace(/[`~ !@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').toLowerCase();
+  const letters = Array.from(x);
+ 
+  if (letters[0] == letters[letters.length - 1]){
     return true;
   }
+  else{
+    return false;
+  }
+}
   
-  palindrome("eye");
+  
+palindrome("eye");
