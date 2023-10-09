4945fb67316e515ffa60492aa966183152ca7030
Shariar Rafi <shariar.rafi364@gmail.com>
2023-10-09 23:48:19 +0600

    "09/10/23"

--- Basic Java Script Codes/Number_Method.js
+++ Basic Java Script Codes/Number_Method.js
@@ -1,10 +1,10 @@
-/*
+/*number method of java script*/
 let x = 9.656;
 x.toFixed(0); //10
 x.toFixed(2); //9.66
 x.toFixed(4); //9.6560
 x.toFixed(6); //9.656000
-*/
+
 
 /*
 Number(true);
@@ -18,4 +18,5 @@ Number("10,33");
 Number("10 33");
 Number("John");
  */
+
 
