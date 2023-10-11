dd754b74efab8a4130bc58eb24b803c4ca137617
Shariar Rafi <shariar.rafi364@gmail.com>
2023-10-12 00:55:55 +0600

    12/10/23 4th commit

--- Basic Java Script Codes/For_of_loop.js
+++ Basic Java Script Codes/For_of_loop.js
@@ -1,7 +1,7 @@
 let myArray = [ 1,12,2,0,58,100];
 
 
-// for of loop best of accessing array elements 
+// for of loop  is best of accessing array elements 
 for(x of myArray){
     console.log(x);
 }
---
+++ Basic Java Script Codes/While_Loop.js
@@ -0,0 +1,16 @@
+let i = 0;
+let text = "";
+
+
+while (i<10){
+    text += "The number is "+i;
+    i++;
+}
+
+
+// do {
+//     text += "The number is "+i;
+//     i++;
+// } while (i<10); //rarely used
+
+console.log(text);
