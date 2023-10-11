ae56d4f85487cf911b5e3d23c38fb3bec0a45968
Shariar Rafi <shariar.rafi364@gmail.com>
2023-10-12 01:09:33 +0600

    12/10/23 5th commit

---
+++ Basic Java Script Codes/JS_break_continue.js
@@ -0,0 +1,20 @@
+/*
+//use of break statement
+for(let i=0;i<=10;i++){
+    if(i===5){
+        break;
+    }
+console.log(i); // 0 1 2 3 4 
+};
+*/
+
+
+
+
+//use of continue statement 
+for(let i=0;i<=10;i++){
+    if(i===5){
+        continue; //skips 5 no index
+    }
+console.log(i); // 0 1 2 3 4 6 7 8 9 10
+};
