c7ded6e969a29f0a357dcc71a4caf25dfa6d19f6
Shariar Rafi <shariar.rafi364@gmail.com>
2023-10-12 00:23:06 +0600

    12/10/23 2nd commit

--- Basic Java Script Codes/For_in_loop.js
+++ Basic Java Script Codes/For_in_loop.js
@@ -1,9 +1,8 @@
 const person = { fname: "Shariar",lname : "Rafi", age : 23};
 
-let y = [1,2,4,5,"end",5.23];
+// let y = [1,2,4,5,"end",5.23];
 
-for(let x in y ){
-
-    console.log(y[x]);
-
+//for in loop
+for(let x in person ){
+    console.log(person[x]); 
 }
