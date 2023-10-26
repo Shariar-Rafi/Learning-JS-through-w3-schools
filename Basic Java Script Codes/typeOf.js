f1579ee9671f774972e92ba44b10b8ec5205a95e
Shariar Rafi <shariar.rafi364@gmail.com>
2023-10-26 19:16:26 +0600

    26/10/23 1st

---
+++ Basic Java Script Codes/typeOf.js
@@ -0,0 +1,25 @@
+// a = 71;
+// b = "BD";
+
+// c = b+" "+a;
+
+// console.log(c);
+// console.log(typeof c);
+
+
+/**
+ a = 71;
+d = 9;
+b = "BD";
+
+c = " "+b+" "+a+d;
+
+console.log(c);
+console.log(typeof c); 
+ */
+
+/*
+a = 13;
+console.log(typeof a);
+console.log(typeof typeof a);
+*/
