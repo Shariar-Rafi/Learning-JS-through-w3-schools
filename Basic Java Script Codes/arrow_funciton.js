2ad7ef639605ab7ab33ba7ef0651a714004ce2c9
Shariar Rafi <shariar.rafi364@gmail.com>
2023-10-27 17:04:05 +0600

    27/10/23 3rd

---
+++ Basic Java Script Codes/arrow_funciton.js
@@ -0,0 +1,15 @@
+// function A(){
+    // return "hello";
+// };
+
+// A = function (){
+    // return "hello";
+// };
+
+// A = () => {
+//     return "hello";
+// }
+
+const A = () => "hello"; //returns hello 
+
+
