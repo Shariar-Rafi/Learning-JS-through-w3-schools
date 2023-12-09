cf0aa32c8018cae3daba556b55c9401d4bc59c1c
Shariar Rafi <shariar.rafi364@gmail.com>
2023-12-10 01:09:16 +0600

    updated

--- Basic Java Script Codes/promisePrac1.js
+++ Basic Java Script Codes/promisePrac1.js
@@ -1,5 +1,63 @@
+//promise
 const paySuccess = true;
-const marks = 70;
+const marks = 80;
 
- 
+function enroll(){
+    console.log("Course enrollment in progress...");
 
+    const promise = new Promise(function(resolve,reject){
+        
+    setTimeout(
+        function(){
+            if(paySuccess){
+                resolve();
+            }
+            else{
+                reject("Payment failed!");
+            }
+        },2000
+    );
+    });
+
+        return promise;
+}
+
+function progress(){
+    console.log("Coure on progress...")
+
+    const promise = new Promise(function(resolve,reject){
+        setTimeout(function(){
+            if(marks >= 80){
+                resolve();
+            } else{
+                reject("Not Enough marks");
+            };
+        },3000);
+    });
+
+    return promise;
+}
+
+//the work is finished here, that's why we didn't declare any parameter
+function getCertificate(){
+    console.log("Preparing certificate...");
+
+    const promise = new Promise(function(resolve){
+        setTimeout(function(){
+            resolve("You got the certificate :)");
+        },1000);
+    });
+
+  return promise;
+}
+
+//calling 
+enroll()
+    .then(progress)
+    .then(getCertificate)
+    .then(function(value){
+        console.log(value);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
