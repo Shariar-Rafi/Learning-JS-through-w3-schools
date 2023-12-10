05f80373364e7f70cd6cca122f9bd761f9670983
Shariar Rafi <shariar.rafi364@gmail.com>
2023-12-11 00:27:57 +0600

    updated

--- JS HTML DOM/DOM Demo 2/script.js
+++ JS HTML DOM/DOM Demo 2/script.js
@@ -1,2 +1,25 @@
 // document.getElementById("img1").src = "pic_bulbon.gif";
 
+/*
+function validateForm(){
+    const f = document.forms["myForm"] ["fname"].value;
+    if(f === ""){
+        alert("Enter your name correctly!");
+        return false;
+    }
+}
+*/
+let text;
+function validateForm(){
+    let f = document.getElementById("numb");
+    let n = f.value;
+
+    if(isNaN(n) || n>10 || n<1 ){
+        text = "Invalid input!!!"
+    }
+    else{
+        text  = "Verified input"
+    }
+
+    document.getElementById("demo").innerHTML = text;
+}
