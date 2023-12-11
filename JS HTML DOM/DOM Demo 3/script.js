7ff5fe85f97bedc414b12ea6fd2da7cb20d5eb5d
Shariar Rafi <shariar.rafi364@gmail.com>
2023-12-11 17:02:02 +0600

    updated

--- JS HTML DOM/DOM Demo 3/script.js
+++ JS HTML DOM/DOM Demo 3/script.js
@@ -1,17 +1,5 @@
-/*
-// document.getElementById("demo").innerHTML="Test 0";
-const p = document.getElementById("demo");
-p.innerHTML="Test 0";
-*/
-
-
-const x = document.forms["frm1"];
-
-let text = "";
-//looping into the targeted form 
-for (let i = 0; i < x.length; i++) {
-    text+= x.elements[i].value + "</br>";
-}
-
-document.getElementById("demo").innerHTML= text;
-
+setTimeout(function(){
+    const p = document.querySelector("#demo");
+p.style.color = "red";
+p.style.fontSize = "200px";
+},4000)
