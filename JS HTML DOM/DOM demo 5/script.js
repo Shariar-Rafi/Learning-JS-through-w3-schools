070aa6c3e63f19c5750eaa09ff8550c6b46b7520
Shariar Rafi <shariar.rafi364@gmail.com>
2023-12-12 02:22:20 +0600

    demo 5 updated

---
+++ JS HTML DOM/DOM demo 5/script.js
@@ -0,0 +1,10 @@
+function changeText(){
+    document.getElementById("b1").innerHTML=
+    "Thank you for clicking!";
+}
+
+
+const button = document.querySelector("#b3");
+button.onclick = function(){
+    button.innerHTML="Hola"
+}
